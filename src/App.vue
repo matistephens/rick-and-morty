ee389be01ada46724caa01a5a705db31fe95445b
Matias Stephens <mstephens@riafinancial.com>
2022-05-02 20:30:31 -0600

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,70 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <section class="center">
+    <div>
+      <img class="logo" src="./assets/rick-and-morty-logo.svg" alt="logo">
+      <div class="list">
+        <Personaje v-for="(personaje, $index) in personajes" :key="$index" :nombrePersonaje= personaje.nombre :imagenPersonaje= personaje.imagen />
+      </div>
+      
+    </div>
+  </section>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Personaje from "@/components/Personaje.vue";
 
 export default {
   name: 'App',
+  data: () => ({
+    personajes: [
+      { 
+        nombre: "Rick Sanchez", 
+        imagen:"https://rickandmortyapi.com/api/character/avatar/1.jpeg"
+      },
+      { 
+        nombre: "Morty Smith", 
+        imagen:"https://rickandmortyapi.com/api/character/avatar/2.jpeg"
+      },
+      { 
+        nombre: "Summer Smith", 
+        imagen:"https://rickandmortyapi.com/api/character/avatar/3.jpeg"
+      },
+      { 
+        nombre: "Beth Smith", 
+        imagen:"https://rickandmortyapi.com/api/character/avatar/4.jpeg"
+      },
+    ]
+
+  }),
   components: {
-    HelloWorld
+    Personaje
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  margin-top: 64px;
+  background-image: url("assets/space.png");
+  background-size: cover;
+  height: 100vh;
 }
+
+.center{
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+
+}
+.logo {
+  width: 30vw;
+  margin-bottom: 24px;
+  min-width: 300px;
+}
+.list {
+  display: flex;
+  flex-direction: column;
+}
+
 </style>
---
+++ src/components/Personaje.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="listItem">
+      <img class="avatar" :src="imagenPersonaje"  alt="">
+      <h1>{{nombrePersonaje}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'personaje',
+    props: {
+        nombrePersonaje: String,
+        imagenPersonaje: String,
+    },
+}
+</script>
+
+<style>
+h1 {
+    color: white;
+    text-shadow: 0 0 20px #74b13d;
+}
+.avatar {
+    border-radius: 50%;
+    width: 72px;
+}
+.listItem {
+    display: flex;
+    gap: 1rem;
+    padding: 1rem 1rem 1rem 1rem;
+}
+</style>
